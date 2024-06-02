54b793b274d7feb23ec9ec72685d3b353c7e3903
hassanyassin <hassanyassin.dev@gmail.com>
2024-06-02 22:12:20 +0300

    first_push

--- lib/Chat/Screens/User_Chat_Screen.dart
+++ lib/Chat/Screens/User_Chat_Screen.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 import '../../../Global/Functions/Colors.dart';
 import '../../../Global/Widgets/AppBar.dart';
+import '../../../Global/Widgets/Failed.dart';
+
+import '../../../Chat/Continued_Providers/Get_User_Chats.dart';
 
 class User_Chat_Screen extends StatefulWidget {
   const User_Chat_Screen({super.key});
@@ -17,6 +22,26 @@ class _User_Chat_ScreenState extends State<User_Chat_Screen> {
     return Scaffold(
       backgroundColor: Get_White,
       appBar: C_AppBar(title: 'Chats'),
+      body: FutureBuilder(
+        future: Cd_Get_User_Chats(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CupertinoActivityIndicator(
+                radius: 1.5.h,
+                color: Get_Black,
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Failed_Icon_and_Text(
+              is_allow_refresh: true,
+              onTap: () => setState(() {}),
+            );
+          } else {
+            return const SizedBox();
+          }
+        },
+      ),
     );
   }
 }
